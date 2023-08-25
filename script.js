1ffdad678167ae468172098160fb2dcb24f58cb6
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-08-25 14:37:37 -0300

    melhorando o portfólio (adicionando o about-me)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*------BOTÃO HAMBURGER HEADER------*/
 const headerButton = document.querySelector(".header__button");
 const main = document.querySelector("main");
 const headerNav = document.querySelector(".header__nav");
@@ -19,3 +20,36 @@ headerNav.addEventListener("click", () => {
   navHeader.classList = "header__nav header__nav--disappear";
   headerButton.classList.remove("header__button--color");
 });
+
+/*------LINKS HEADER------*/
+const headerLinksClassGeneric = document.querySelector("main").children;
+const buttonHome = document.querySelector("#button__home");
+const buttonAboutMe = document.querySelector("#button__about-me");
+const buttonPortfolio = document.querySelector("#button__portfolio");
+const buttonContact = document.querySelector("#button__contact");
+
+buttonHome.addEventListener("click", () => {
+  const containerHome = document.querySelector("#container__home");
+  for (let i = 0; i < headerLinksClassGeneric.length; i++) {
+    headerLinksClassGeneric[i].classList.add("class__generic");
+  }
+  containerHome.classList.remove("class__generic");
+});
+
+buttonAboutMe.addEventListener("click", () => {
+  const containeraboutMe = document.querySelector("#container__about-me");
+  for (let i = 0; i < headerLinksClassGeneric.length; i++) {
+    headerLinksClassGeneric[i].classList.add("class__generic");
+  }
+  containeraboutMe.classList.remove("class__generic");
+});
+
+/*------LENDO UM ARQUIVO DOC------*/
+const pText = document.querySelector(".text");
+const endPoint = "/curriculo/about-me.doc";
+
+fetch(endPoint)
+  .then((res) => res.text())
+  .then((data) => {
+    pText.innerText = data;
+  });
