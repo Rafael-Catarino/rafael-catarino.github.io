097f86046e1725f5611370509485a77dbc688b89
Rafael Catarino <rafaelscatarino@hotmail.com>
2023-08-25 11:32:55 -0300

    melhorando o portfólio (adicionando o header e a home)

--- script.js
+++ script.js
@@ -2,30 +2,20 @@ const headerButton = document.querySelector(".header__button");
 const main = document.querySelector("main");
 const headerNav = document.querySelector(".header__nav");
 
-headerButton.addEventListener('click', () => {
-  const navHeader = document.querySelector('.header__nav');
-  navHeader.classList.toggle('header__nav--disappear');
-  headerButton.classList.toggle('header__button--color');
+headerButton.addEventListener("click", () => {
+  const navHeader = document.querySelector(".header__nav");
+  navHeader.classList.toggle("header__nav--disappear");
+  headerButton.classList.toggle("header__button--color");
 });
 
-main.addEventListener('click', () => {
-  const navHeader = document.querySelector('.header__nav');
-  navHeader.classList = 'header__nav header__nav--disappear';
-  headerButton.classList.remove('header__button--color');
+main.addEventListener("click", () => {
+  const navHeader = document.querySelector(".header__nav");
+  navHeader.classList = "header__nav header__nav--disappear";
+  headerButton.classList.remove("header__button--color");
 });
 
-headerNav.addEventListener('click', () => {
-  const navHeader = document.querySelector('.header__nav');
-  navHeader.classList = 'header__nav header__nav--disappear';
-  headerButton.classList.remove('header__button--color');
-});
-
-//LENDO UM ARQUIVO DOC
-const pText = document.querySelector(".text");
-const endPoint = "/curriculo/about-me.doc";
-
-fetch(endPoint)
-.then(res=>res.text())
-.then(data=>{
-  pText.innerText = data;
+headerNav.addEventListener("click", () => {
+  const navHeader = document.querySelector(".header__nav");
+  navHeader.classList = "header__nav header__nav--disappear";
+  headerButton.classList.remove("header__button--color");
 });
